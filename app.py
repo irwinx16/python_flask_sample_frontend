9d4967e4b1eb54393dfa3fbd4ada4197d6ed32fd
irwin <irgranados16@gmail.com>
2020-04-22 16:11:28 -0500

    update templates with correct information

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.jinja_env.auto_reload = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Home Page
 @app.route('/')
@@ -13,3 +11,7 @@ def index():
 @app.route('/resume')
 def resume():
     return render_template('resume.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
