8fc9109ab2416143e53dc461dd23b2ec78a5bd72
irwin <irgranados16@gmail.com>
2020-04-26 22:04:28 -0500

    add geolocalization

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     WEATHER_KEY = '8f7e90ae66919f9651f80c29d46c0a45'
-    city = 'Chicago'
+    city = get_geolocation()
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, WEATHER_KEY)
     response = requests.get(url).json()
@@ -15,14 +15,22 @@ def index():
     f_temp = int(((k_temp - 273.15) * 1.8) + 32)
     c_temp = int(k_temp - 273.15)
     icon   = response['weather'][0]['icon']
-    desc   = response['weather'][0]['description']
+    desc   = response['weather'][0]['description'].title()
 
-    return render_template('index.html', temp_f=f_temp, temp_c=c_temp, icon=icon, description=desc)
+    return render_template('index.html', temp_f=f_temp, temp_c=c_temp, icon=icon, description=desc, city=city)
+
 
 # Resume Page
 @app.route('/resume')
 def resume():
     return render_template('resume.html')
+
+# Geo-Location
+def get_geolocation():
+    url = 'http://ip-api.com/json/'
+    response = requests.get(url).json()
+    city = response['city']
+    return city
 
 
 if __name__ == '__main__':
